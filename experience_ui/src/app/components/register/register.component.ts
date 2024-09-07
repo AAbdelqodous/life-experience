04711f651d98fc8c1828faf9086562025e2bdbc4
AAbdelqodous <ml.ahmed1187@gmail.com>
2024-09-07 18:07:21 +0300

    register form

--- experience_ui/src/app/components/register/register.component.ts
+++ experience_ui/src/app/components/register/register.component.ts
@@ -1,12 +1,64 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { passwordMatchValidator } from '../shared/password-match.directive';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    CardModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    CommonModule,
+    RouterModule
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registrationForm: FormGroup;
 
+  // Assign FormBuilder to a class property
+  constructor(private fb: FormBuilder) {
+    // Initialize the form
+    this.registrationForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(?:[a-zA-Z]+)*$/)]],
+      lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(?:[a-zA-Z]+)*$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validators: passwordMatchValidator  // Add custom validator here
+    });    
+  }
+
+  get firstName(){
+    return this.registrationForm.controls['firstName'];
+  }
+
+  get lastName(){
+    return this.registrationForm.controls['lastName'];
+  }
+
+  get email(){
+    return this.registrationForm.controls['email'];
+  }
+
+  get password(){
+    return this.registrationForm.controls['password'];
+  }
+
+  get confirmPassword(){
+    return this.registrationForm.controls['password'];
+  }
+
+  submitUser(){
+    
+  }
 }
--- experience_ui/src/app/services/auth.service.ts
+++ experience_ui/src/app/services/auth.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../interfaces/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private baseUrl = 'http://localhost:8088/api/v1/auth';
+
+  constructor(private http: HttpClient) { }
+
+  register(user: User){
+      return this.http.post(`${this.baseUrl}/register`, user);
+  }
 }
